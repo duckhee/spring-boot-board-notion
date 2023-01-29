83ec012becf892734178edb45519d1f5175cc13a
DoukheeWon <fain9301@yahoo.com>
2023-01-29 22:28:38 +0900

    feat: set appliction pid file set

--- src/main/java/kr/co/won/simpleboard/NotionSimpleBoardApplication.java
+++ src/main/java/kr/co/won/simpleboard/NotionSimpleBoardApplication.java
@@ -2,12 +2,17 @@ package kr.co.won.simpleboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class NotionSimpleBoardApplication {
 
+
     public static void main(String[] args) {
-        SpringApplication.run(NotionSimpleBoardApplication.class, args);
+        SpringApplication application = new SpringApplication(NotionSimpleBoardApplication.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
+
     }
 
 }
