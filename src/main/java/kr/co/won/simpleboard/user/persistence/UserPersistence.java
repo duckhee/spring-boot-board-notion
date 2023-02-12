4f183ecf13728cc5d9e22c0bfe737b29b4c13635
DoukheeWon <fain9301@yahoo.com>
2023-02-12 23:42:28 +0900

    feat:#11-user
    user registered page set

--- src/main/java/kr/co/won/simpleboard/user/persistence/UserPersistence.java
+++ src/main/java/kr/co/won/simpleboard/user/persistence/UserPersistence.java
@@ -3,5 +3,17 @@ package kr.co.won.simpleboard.user.persistence;
 import kr.co.won.simpleboard.user.domain.UserDomain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserPersistence extends JpaRepository<UserDomain, Long> {
+
+    Optional<UserDomain> findByEmail(String email);
+
+    Optional<UserDomain> findByUserId(String userId);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByUserId(String userId);
+
+    boolean existsByEmailAndUserId(String email, String userId);
 }
