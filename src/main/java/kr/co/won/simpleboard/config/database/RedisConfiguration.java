cea1586f253a0983b180c4c4c9e75cf70e1fb81e
DoukheeWon <fain9301@yahoo.com>
2023-02-26 17:40:24 +0900

    feat:#20-userService
     add cache redis setting test

--- src/main/java/kr/co/won/simpleboard/config/database/RedisConfiguration.java
+++ src/main/java/kr/co/won/simpleboard/config/database/RedisConfiguration.java
@@ -1,4 +1,38 @@
 package kr.co.won.simpleboard.config.database;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+@EnableRedisRepositories(value = {
+
+})
+@Configuration
+@RequiredArgsConstructor
 public class RedisConfiguration {
+
+
+    @Bean
+    public LettuceConnectionFactory lettuceConnectionFactory(RedisProperties redisProperties) {
+        return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory lettuceConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new GenericToStringSerializer<>(Object.class));
+        redisTemplate.setHashValueSerializer(new JdkSerializationRedisSerializer());
+        redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
+        redisTemplate.setConnectionFactory(lettuceConnectionFactory);
+        return redisTemplate;
+    }
+
+
 }
